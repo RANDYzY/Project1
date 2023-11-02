ce5f50351bf8d88a74992b9c50584eff8a85a301
RANDYzY <maximusismax@gmail.com>
2023-11-02 16:22:27 +0300

    Five commit

--- Project11/Project11.cpp
+++ Project11/Project11.cpp
@@ -3,25 +3,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
     cout << "Buy my paket!" << endl;
     cout << "Close eyes!" << endl;
     cout << "Buy my pakets!" << endl;
