fcb55c11550b8bf57b8a94a058e25766cebba9a4
RANDYzY <maximusismax@gmail.com>
2023-11-02 15:56:23 +0300

    First Commit

---
+++ Project11/Project11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    //...
+}
